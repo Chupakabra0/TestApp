30ef82bd3c360232ae525b7f02357317321b07c3
Chupakabra0 <rootalfa4@gmail.com>
2021-11-23 20:16:14 +0200

    Add: большое обновление в отображении теории и вопросов

--- TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
+++ TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
@@ -1,10 +1,44 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Markup;
 using TestApp.Core.Commands.RelayCommand;
 
 namespace TestApp.MVVM.ViewModels.TestViewModel {
-    class TestViewModel : BaseViewModel.BaseViewModel {
-        public ICommand ButtonCommand
-            => new RelayCommand(() => MessageBox.Show("It works perfectly!", "YES!!!"));
+    public class TestViewModel : BaseViewModel.BaseViewModel {
+        public TestViewModel() {
+
+        }
+
+        public ICommand OpenSavageCommand =>
+            new RelayCommand(() => {
+                // TODO: savage test generate
+                this.IsTestChosen = true;
+            });
+
+        public ICommand OpenPearsonCommand =>
+            new RelayCommand(() => {
+                // TODO: pearson test generate
+                this.IsTestChosen = true;
+            });
+
+        public ICommand BackCommand =>
+            new RelayCommand(() => {
+                this.IsTestChosen = false;
+            });
+
+        public bool IsTestChosen      { get; set; }
+        public bool IsMenuEnabled  => !this.IsTestChosen;
+
+        //--------------------- TESTS -----------------------------//
+
+
+
+        //--------------------- MULTITESTS -----------------------------//
+
+
+
+        //--------------------- QUESTION -----------------------------//
+
+
     }
 }
