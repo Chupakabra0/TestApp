1fd0dbac0e25579837f7be3c079096fc3b18c935
Chupakabra0 <rootalfa4@gmail.com>
2021-11-25 01:12:20 +0200

    Add: создан шаблон для вопросов

--- TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
+++ TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
@@ -33,28 +33,30 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
         //--------------------- TESTS -----------------------------//
 
         private string desc_1 =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><TextBlock Style=\"{StaticResource Content}\">Нехай маємо матрицю S. Знайдіть (вкажіть номер), чому дорівнює α<Run BaselineAlignment=\"TextTop\">*</Run>, якщо матриця S має наступний вигляд:</TextBlock><Controls:FormulaControl Style=\"{StaticResource BasicFormula}\" HorizontalAlignment=\"Center\" Formula=\"Q = \\pmatrix{7 &amp; 2 &amp; 8 \\cr 3 &amp; 9 &amp; 4 \\cr 1 &amp; 6 &amp; 0 }\" Margin=\"20\" Scale=\"25\"></Controls:FormulaControl></StackPanel>";
+            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><TextBlock Style=\"{StaticResource Content}\" Text=\"За якою формулою можна знайти значення точки, що відповідає розв’язку в алгоритмі пошуку рандомізованого розв'язку за критерієм Неймана-Пірсона?\"></TextBlock></StackPanel>";
         public object RB_Desc_1 => XamlReader.Parse(this.desc_1);
 
         private string a1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl Scale=\"20\" Style=\"{StaticResource BasicFormula}\" Formula=\"x l'_{i 1} + (1 - x) l'_{j 1} = x l'_{i 2} + (1 - x) l'_{j 2}\"></Controls:FormulaControl></StackPanel>";
+            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } - (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
         public object RB_a1_Content => XamlReader.Parse(this.a1_content);
         public bool RB_a1_Checked { get; set; }
 
         private string b1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl Scale=\"20\" Style=\"{StaticResource BasicFormula}\" Formula=\"x l'_{i 1} + (1 - x) l'_{j 1} = 0\"></Controls:FormulaControl></StackPanel>";
+            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } + (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
         public object RB_b1_Content => XamlReader.Parse(this.b1_content);
         public bool RB_b1_Checked { get; set; }
 
         private string c1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><TextBlock Style=\"{StaticResource BoxText}\">По осі абсцис відкладаємо втрати, відповідні β<Run BaselineAlignment=\"Subscript\">2</Run>, по осі ординат відповідні втрати при β<Run BaselineAlignment=\"Subscript\">1</Run></TextBlock></StackPanel>";
+            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"(1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } -x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
         public object RB_c1_Content => XamlReader.Parse(this.c1_content);
         public bool RB_c1_Checked { get; set; }
 
         private string d1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><TextBlock Style=\"{StaticResource BoxText}\">По осі абсцис відкладаємо втрати, відповідні β<Run BaselineAlignment=\"Subscript\">2</Run>, по осі ординат відповідні втрати при β<Run BaselineAlignment=\"Subscript\">1</Run></TextBlock></StackPanel>";
+            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } + (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = 0 \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
         public object RB_d1_Content => XamlReader.Parse(this.d1_content);
         public bool RB_d1_Checked { get; set; }
+
+        public bool CheckMark_1 => this.RB_d1_Checked;
 
         //--------------------- MULTITESTS -----------------------------//
 
