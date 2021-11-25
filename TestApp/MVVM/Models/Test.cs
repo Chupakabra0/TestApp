3e8ce5c681294026d03ed812bb2db30ff70cb175
TretiakLena <60944307+TretiakLena@users.noreply.github.com>
2021-11-25 21:51:07 +0200

    Fix

--- TestApp/MVVM/Models/Test.cs
+++ TestApp/MVVM/Models/Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Markup;
 
 namespace Models
 {
@@ -9,43 +10,44 @@ namespace Models
         public Test(Common test)
         {
             id = test.id;
-            description = test.description;
+            description = XamlReader.Parse(test.description);
+            //explanation = XamlReader.Parse(test.explanation);
             explanation = test.explanation;
-            variantA = test.variantA.text;
-            variantB = test.variantB.text;
-            variantC = test.variantC.text;
-            variantD = test.variantD.text;
+            variantA = XamlReader.Parse(test.variantA.text);
+            variantB = XamlReader.Parse(test.variantB.text);
+            variantC = XamlReader.Parse(test.variantC.text);
+            variantD = XamlReader.Parse(test.variantD.text);
         }
         public Test(Multitest test)
         {
             id = test.id;
-            description = test.description;
-            explanation = test.explanation;
-            variantA = test.variantA.text;
-            variantB = test.variantB.text;
-            variantC = test.variantC.text;
-            variantD = test.variantD.text;
+            description = XamlReader.Parse(test.description);
+            explanation = XamlReader.Parse(test.explanation);
+            variantA = XamlReader.Parse(test.variantA.text);
+            variantB = XamlReader.Parse(test.variantB.text);
+            variantC = XamlReader.Parse(test.variantC.text);
+            variantD = XamlReader.Parse(test.variantD.text);
         }
         public Test(Quiz test)
         {
             id = test.id;
-            description = test.description;
-            explanation = test.explanation;
+            description = XamlReader.Parse(test.description);
+            explanation = XamlReader.Parse(test.explanation);
             answer = test.answer;
         }
         public int id { get; set; }
 
-        public string description { get; set; }
+        public object description { get; set; }
 
-        public string explanation { get; set; }
+        public object explanation { get; set; }
 
-        public string variantA { get; set; }
+        public object variantA { get; set; }
 
-        public string variantB { get; set; }
+        public object variantB { get; set; }
 
-        public string variantC { get; set; }
+        public object variantC { get; set; }
 
-        public string variantD { get; set; }
+        public object variantD { get; set; }
         public string answer { get; set; }
     }
 }
