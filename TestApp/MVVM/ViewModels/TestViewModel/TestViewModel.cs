fbc4ab5c17a0f9ebcb167860be209d8b4be6585b
Chupakabra0 <rootalfa4@gmail.com>
2021-11-26 13:31:17 +0200

    Fix: классный своп

--- TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
+++ TestApp/MVVM/ViewModels/TestViewModel/TestViewModel.cs
@@ -69,9 +69,7 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
             {
                 j = random.Next(i + 1);
                 rand_common.Add(j);
-                var tmp = this.obj_common[j];
-                this.obj_common[j] = this.obj_common[i];
-                this.obj_common[i] = tmp;
+                (this.obj_common[j], this.obj_common[i]) = (this.obj_common[i], this.obj_common[j]);
             }
         }
         public void MixMultiTest()
@@ -82,9 +80,7 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
             {
                 j = random.Next(i + 1);
                 rand_multitest.Add(j);
-                var tmp = this.obj_multitest[j];
-                this.obj_multitest[j] = this.obj_multitest[i];
-                this.obj_multitest[i] = tmp;
+                (this.obj_multitest[j], this.obj_multitest[i]) = (this.obj_multitest[i], this.obj_multitest[j]);
             }
         }
         public void MixQuiz()
@@ -95,9 +91,7 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
             {
                 j = random.Next(i + 1);
                 rand_quiz.Add(j);
-                var tmp = this.obj_quiz[j];
-                this.obj_quiz[j] = this.obj_quiz[i];
-                this.obj_quiz[i] = tmp;
+                (this.obj_quiz[j], this.obj_quiz[i]) = (this.obj_quiz[i], this.obj_quiz[j]);
             }
         }
         #endregion
@@ -115,32 +109,10 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
         public Test test5 { get; set; }
         public Test test6 { get; set; }
 
-
-
-        private string desc_1 =
-                "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><TextBlock Style=\"{StaticResource Content}\" Text=\"За якою формулою можна знайти значення точки, що відповідає розв’язку в алгоритмі пошуку рандомізованого розв'язку за критерієм Неймана-Пірсона?\"></TextBlock></StackPanel>";
-        public object RB_Desc_1 => XamlReader.Parse(this.desc_1);
-
-        private string a1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } - (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
-        public object RB_a1_Content => XamlReader.Parse(this.a1_content);
         public bool RB_a1_Checked { get; set; }
-
-        private string b1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } + (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
-        public object RB_b1_Content => XamlReader.Parse(this.b1_content);
         public bool RB_b1_Checked { get; set; }
-
-        private string c1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"(1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } -x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } = \\pmatrix{ L_{1} \\cr L_{2} } \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
-        public object RB_c1_Content => XamlReader.Parse(this.c1_content);
         public bool RB_c1_Checked { get; set; }
-
-        private string d1_content =
-            "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:Controls=\"clr-namespace:WpfMath.Controls;assembly=WpfMath\"><Controls:FormulaControl HorizontalAlignment=\"Center\" Style=\"{StaticResource BasicFormula}\" Formula=\"x_{i} \\pmatrix{ l_{i 1} \\cr l_{i 2} } + (1 - x_{i}) \\pmatrix{ l_{j 1} \\cr l_{j 2} } = 0 \" Margin=\"0\" Scale=\"20\"></Controls:FormulaControl></StackPanel>";
-        public object RB_d1_Content => XamlReader.Parse(this.d1_content);
         public bool RB_d1_Checked { get; set; }
-
 
         public bool CheckMark_1 => this.RB_d1_Checked;
 
@@ -154,6 +126,9 @@ namespace TestApp.MVVM.ViewModels.TestViewModel {
         public Test test10 { get; set; }
         public Test test11 { get; set; }
         public Test test12 { get; set; }
-#endregion
+
+        #endregion
+
+        public List<Tuple<bool, bool, bool, bool>> bools;
     }
 }
