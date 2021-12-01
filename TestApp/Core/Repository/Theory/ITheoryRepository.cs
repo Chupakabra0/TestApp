a4978507a7936fb7a4e4649fe38836e057225a0e
Chupakabra0 <rootalfa4@gmail.com>
2021-12-01 18:15:56 +0200

    Add: вся теория и хедеры для неё готовы

--- TestApp/Core/Repository/Theory/ITheoryRepository.cs
+++ TestApp/Core/Repository/Theory/ITheoryRepository.cs
@@ -4,5 +4,8 @@ namespace TestApp.Core.Repository.Theory {
     public interface ITheoryRepository {
         public FlowDocument GetSavageTheory();
         public FlowDocument GetPearsonTheory();
+
+        public string GetSavageName();
+        public string GetPearsonName();
     }
 }
--- TestApp/MVVM/ViewModels/TheoryViewModel/TheoryViewModel.cs
+++ TestApp/MVVM/ViewModels/TheoryViewModel/TheoryViewModel.cs
@@ -10,25 +10,29 @@ namespace TestApp.MVVM.ViewModels.TheoryViewModel
             this.TheoryRepository = DummyTheoryRepository.Instance;
         }
 
-        public FlowDocument Document { get; set; }
+        public FlowDocument Document     { get; set; }
+        public string       DocumentName { get; set; }
 
         public ITheoryRepository TheoryRepository { get; set; }
 
         public ICommand OpenSavageCommand =>
             new RelayCommand(() => {
                 this.Document       = this.TheoryRepository?.GetSavageTheory();
+                this.DocumentName   = this.TheoryRepository?.GetSavageName();
                 this.IsTheoryChosen = true;
             });
         
         public ICommand OpenPearsonCommand =>
             new RelayCommand(() => {
                 this.Document       = this.TheoryRepository?.GetPearsonTheory();
+                this.DocumentName   = this.TheoryRepository?.GetPearsonName();
                 this.IsTheoryChosen = true;
             });
 
         public ICommand BackCommand =>
             new RelayCommand(() => {
                 this.Document       = null;
+                this.DocumentName   = null;
                 this.IsTheoryChosen = false;
             });
 
